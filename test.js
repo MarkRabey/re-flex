a3c79c2b27d8fa3a1977a9c0ecd3892152115f20
Mark <mark@markrabey.com>
2017-07-31 15:09:58 -0400

    remove FlexItem from test

--- test.js
+++ test.js
@@ -2,9 +2,8 @@ import test from 'ava';
 import React from 'react';
 import { create } from 'react-test-renderer';
 
-import { Flex, FlexItem } from './src';
+import Flex from './src';
  
 test('exports components', t => {
   t.is(typeof Flex, 'function');
-  t.is(typeof FlexItem, 'function');
 });
